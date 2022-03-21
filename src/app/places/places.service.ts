1b0087fc4c1831d04ef66c8a4d2f8de45a547bee
Kevin-Lejava <klejava@gmail.com>
2022-03-21 13:57:33 -0400

    added input fields for login page and spinner to simulate loading.

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -9,7 +9,7 @@ export class PlacesService {
       "p1",
       "Manhattan Mansion",
       "In the heart of New York City.",
-      "placeholder",
+      "https://img.thedailybeast.com/image/upload/c_crop,d_placeholder_euli9k,h_1440,w_2560,x_0,y_0/dpr_1.5/c_limit,w_1044/fl_lossy,q_auto/v1541016891/181031-carrier-payne-tease_tk805s",
       149.99
     ),
 
@@ -17,7 +17,7 @@ export class PlacesService {
       "p2",
       "PlaceHolder",
       "PlaceHolder",
-      "placeholder",
+      "https://img.thedailybeast.com/image/upload/c_crop,d_placeholder_euli9k,h_1440,w_2560,x_0,y_0/dpr_1.5/c_limit,w_1044/fl_lossy,q_auto/v1541016891/181031-carrier-payne-tease_tk805s",
       999.99
     )
   ];
